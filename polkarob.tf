b0a16418597d0f530eb97a9b76217192419d360d
Robert Frantl <rfrantl@icloud.com>
2023-12-09 08:00:26 +0200

    Saving terraform changes

--- polkarob.tf
+++ polkarob.tf
@@ -132,12 +132,12 @@ resource "aws_instance" "polkanode" {
     type = "ssh"
     user = local.ssh_user
     private_key = file(local.private_key_path)
-    host = aws_instance.polkanodetest.public_ip
+    host = aws_instance.polkanode[count.index].public_ip
    }
 }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i  $(aws_instance.polkanode.public_ip), --private-key $(local.private_key_path) polka.yaml"
+    command = "ansible-playbook -i  $(aws_instance.polkanode[count.index].public_ip), --private-key $(local.private_key_path) polka.yaml"
   
 }
 }
